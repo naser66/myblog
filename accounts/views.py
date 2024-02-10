685c3db580896b667962c53b9146dac08f1d42ce
naser66 <zagros.k66@gmail.com>
2024-02-11 00:35:01 +0330

    add login & logout & signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
